3729087224a6c9f011bc1e59686fc74a3b22b7c5
Kevin Loughead <kvnloughead@gmail.com>
2026-07-03 21:35:49 -0400

    feat: install AMQP and connect to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	rbtURL := "amqp://guest:guest@localhost:5672/"
+	rbtConn, err := amqp.Dial(rbtURL)
+	if err != nil {
+		log.Fatal("Failed to connect to RabbitMQ")
+	}
+	defer rbtConn.Close()
+	fmt.Println("RabbitMQ connection successful")
+
+	_, err = rbtConn.Channel()
+	if err != nil {
+		log.Fatal("Failed to create rabbit channel")
+	}
+
 	fmt.Println("Starting Peril server...")
+
+	// Block until Ctrl+C is received
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("RabbitMQ connection closed")
+
 }
