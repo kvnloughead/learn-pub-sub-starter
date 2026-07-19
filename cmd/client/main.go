7a88d2fc9a2a1e74d35d9be1cc52bb2b33573656
Kevin Loughead <kvnloughead@gmail.com>
2026-07-19 18:45:57 -0400

    declare and bind a pause queue when the client starts

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	rbtURL := "amqp://guest:guest@localhost:5672/"
+	rbtConn, err := amqp.Dial(rbtURL)
+	if err != nil {
+		log.Fatal("Failed to connect to RabbitMQ")
+	}
+	defer rbtConn.Close()
+	fmt.Println("RabbitMQ connection successful")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal("Failed to receive username")
+	}
+
+	pauseQueueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+
+	_, _, err = pubsub.DeclareAndBind(rbtConn, routing.ExchangePerilDirect, pauseQueueName, routing.PauseKey, pubsub.TransientQueue)
+	if err != nil {
+		log.Fatal("Failed to declare and bind the pause queue")
+	}
+
 	fmt.Println("Starting Peril client...")
+
+	// Block until Ctrl+C is received
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("RabbitMQ connection closed")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,9 +3,61 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"log"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+type SimpleQueueType int
+
+const (
+	DurableQueue SimpleQueueType = iota
+	TransientQueue
+)
+
+var SimpleQueueTypeMap = map[SimpleQueueType]string{
+	DurableQueue:   "durable",
+	TransientQueue: "transient",
+}
+
+func (sqt SimpleQueueType) String() string {
+	return SimpleQueueTypeMap[sqt]
+}
+
+// DeclareAndBind opens a new channel on the received connection, declares a
+// queue with the given name and queue type, and binds it to the exhange,
+// returning the channel and the queue.
+//
+// If queueType is durable, autodelete and exclusive are false. If queueType is
+// transient, autodelete and exclusive are true. noWait is always false.
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+
+	rabbitChan, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Failed to create rabbit channel")
+	}
+
+	isDurable := queueType == DurableQueue
+	isTransient := queueType == TransientQueue
+
+	queue, err := rabbitChan.QueueDeclare(queueName, isDurable, isTransient, isTransient, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = rabbitChan.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return rabbitChan, queue, nil
+}
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	jsonBytes, err := json.Marshal(val)
