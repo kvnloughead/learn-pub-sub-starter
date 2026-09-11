0f14fd6b67b36d862fbf7ab617deeb6652518a58
Kevin Loughead <kvnloughead@gmail.com>
2026-09-11 11:19:17 -0400

    set up RabbitMQ connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	rbtConnStr := "amqp://guest:guest@localhost:5672/"
+
+	rbtConn, err := amqp.Dial(rbtConnStr)
+	if err != nil {
+		log.Fatal("Failed to connect to RabbitMQ")
+	}
+	defer rbtConn.Close()
+
+	fmt.Println("Connected to RabbitMQ on port 5672")
 	fmt.Println("Starting Peril server...")
+
+	// Shutdown on Ctrl+C
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("\nShutting down Peril server")
 }
